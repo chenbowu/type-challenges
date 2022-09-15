3abb345c91e4200a28d22230d8d4221cf6e3e25e
以泉印梨花 <31858597+chenbowu@users.noreply.github.com>
2022-09-15 15:14:45 +0800

    learn merge

--- questions/00004-easy-pick/test.cases.ts
+++ questions/00004-easy-pick/test.cases.ts
@@ -21,3 +21,33 @@ interface Expected2 {
   title: string
   completed: boolean
 }
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+}
+
+interface Expected2 {
+  title: string
+  completed: boolean
+}
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+}
+
+interface Expected2 {
+  title: string
+  completed: boolean
+}
