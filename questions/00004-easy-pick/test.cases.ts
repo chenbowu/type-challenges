3f0404af84f1b4ce1841914f1eec62b05a348f38
chenboowu <1028505817@qq.com>
2022-09-15 16:52:09 +0800

    chore: merge

--- questions/00004-easy-pick/test.cases.ts
+++ questions/00004-easy-pick/test.cases.ts
@@ -7,66 +7,6 @@ type cases = [
   MyPick<Todo, 'title' | 'completed' | 'invalid'>,
 ]
 
-type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
-  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
-  // @ts-expect-error
-  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
-]
-
-type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
-  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
-  // @ts-expect-error
-  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
-]
-
-interface Todo {
-  title: string
-  description: string
-  completed: boolean
-}
-
-interface Expected1 {
-  title: string
-}
-type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
-  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
-  // @ts-expect-error
-  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
-]
-
-interface Expected2 {
-  title: string
-  completed: boolean
-}
-type cases = [
-  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
-  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
-  // @ts-expect-error
-  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
-]
-
-interface Todo {
-  title: string
-  description: string
-  completed: boolean
-}
-
-interface Expected1 {
-  title: string
-}
-
-interface Expected1 {
-  title: string
-}
-
-interface Expected2 {
-  title: string
-  completed: boolean
-}
-
 interface Todo {
   title: string
   description: string
