05fefe260ce97c693f054b6e6aebeb899bad000e
chenboowu <1028505817@qq.com>
2022-10-23 14:39:13 +0800

    chore: update

--- challenges/00014-easy-first/template.ts
+++ challenges/00014-easy-first/template.ts
@@ -1,16 +1,10 @@
+// solution 1
 // type First<T extends any[]> = T["length"] extends 0 ? never : T[0]
-// type First<T extends any[]> = T extends infer T ? 
 
-// function describePerson(person: {
-//   name: string;
-//   age: number;
-//   hobbies: [string, string]; // tuple
-// }) {
-//   return `${person.name} is ${person.age} years old and love ${person.hobbies.join(" and  ")}.`;
-// }
+// solution 2
+// infer R 推断出数组的第一个类型，...infer _ 推断出剩余元素的类型，由于用不上所以用_将其丢弃
+type First<T extends any[]> = T extends [infer R, ...infer _] ? R : never;
 
-// const alex = {
-//   name: 'Alex',
-//   age: 20,
-//   hobbies: ['walking', 'cooking'] // type string[] != [string, string]
-// }
+// solution 3
+// 判断 T 是否为一个空数组，如果不是空数组就通过索引取第一个元素的类型
+// type First<T extends any[]> = T extends [] ? never : T[0]
