35f58ab5d6c8b1b949104b3de3899ce934455660
chenboowu <1028505817@qq.com>
2022-10-28 23:53:29 +0800

    chore: update

--- challenges/00009-medium-deep-readonly/template.ts
+++ challenges/00009-medium-deep-readonly/template.ts
@@ -1,3 +1,7 @@
+// 根据示例得出，除了函数类型，其他类型都要设置成 readonly
+// 所以当碰到函数类型的直接返回该函数类型，反之递归调用
+
+// Solution 1
 // type DeepReadonly<T> = {
 //     readonly [Key in keyof T]: T[Key] extends object
 //         ? T[Key] extends Function
@@ -6,14 +10,22 @@
 //         : T[Key]
 // }
 
+// Solution 2
 // Function extends Record<string | number | symbol, any> 成立的
 // 所以当类型再判断类似是是函数类型
-type DeepReadonly<T> = {
-  readonly [Key in keyof T]: T[Key] extends Record<string | number | symbol, any> 
-    ? T[Key] extends Function
-        ? T[Key]
-        : DeepReadonly<T[Key]>
-    : T[Key]
-}
+// type DeepReadonly<T> = {
+//   readonly [Key in keyof T]: T[Key] extends Record<string | number | symbol, any> 
+//     ? T[Key] extends Function
+//         ? T[Key]
+//         : DeepReadonly<T[Key]>
+//     : T[Key]
+// }
+//type t00009 = Function extends Record<string | number | symbol, any> ? true : false
 
-type t00009 = Function extends Record<string | number | symbol, any> ? true : false
+// Solution 3
+type DeepReadonly<T> = {
+    readonly [P in keyof T]: keyof T[P] extends never 
+        ? T[P] 
+        : DeepReadonly<T[P]>
+}
+type t00009 = Function extends never ? true : false
