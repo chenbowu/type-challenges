30e39410e3d581526ddd7d2844fc955e16c0d9a3
chenboowu <1028505817@qq.com>
2022-10-29 22:26:40 +0800

    chore: update

--- challenges/00062-medium-type-lookup/template.ts
+++ challenges/00062-medium-type-lookup/template.ts
@@ -1,3 +1,6 @@
-type LookUp<U, T> = U extends Record<'type', any> 
-    ? U['type'] extends T ? U : never
-    : never
+// type LookUp<U, T> = U extends Record<'type', any> 
+//     ? U['type'] extends T ? U : never
+//     : never
+
+// 这样其实就能实现
+type LookUp<U, T> = U extends { type: T } ? U : never;
