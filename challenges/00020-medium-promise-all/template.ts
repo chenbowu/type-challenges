8cb5da6fb1775d98c18fcef799ae72b306087069
chenboowu <1028505817@qq.com>
2022-10-29 21:58:48 +0800

    chore: update

---
+++ challenges/00020-medium-promise-all/template.ts
@@ -0,0 +1,20 @@
+type AwaitedArray<T extends any[]> = T extends [infer First, ...infer Rest]
+    // AwaitedArray<[1, 2, 3]>
+    // 1. [Awaited<1>, ...AwaitedArray<[2,3]>]
+    // 2. [Awaited<2>, ...AwaitedArray<[3]>]
+    // 3. [Awaited<3>, ...AwaitedArray<[]>]
+    // 4. return []
+    // 替换每次递归的结果 [Awaited<1>, ...[Awaited<2>, ...[Awaited<3>, ...[]]] 
+    // 化简后得到 [Awaited<1>, Awaited<2>, Awaited<3>]
+    ? [Awaited<First>, ...AwaitedArray<Rest>]
+    // 这里必须返回一个空数组
+    : T
+
+declare function PromiseAll<T extends any[]>(
+    // TODO readonly T 会报错, 为什么？
+    // readonly 修饰符只能在数组和元组字面量类型上使用
+    // 所以这里通过展开类型参数 T，计算出一个新的字面量元组类型中
+    // values: readonly any[]       OK
+    // values: readonly T           Error
+    values: readonly [...T]
+): Promise<AwaitedArray<T>> 
