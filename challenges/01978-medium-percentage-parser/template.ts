4c9161ea42308db1d1fa3ebe4478cf817f415839
chenboowu <1028505817@qq.com>
2022-11-12 22:32:45 +0800

    chore: update

--- challenges/01978-medium-percentage-parser/template.ts
+++ challenges/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,12 @@
-type PercentageParser<A extends string> = any
+type Plus = "+" | "-";
+type Percentage = "%";
+
+type GetNumAndUnitTuple<T extends string> = T extends `${infer R}${Percentage}` 
+    ? [R, '%']
+    : [T, '']
+
+type PercentageParser<T extends string> = T extends `${infer F}${infer R}`
+    ? F extends Plus
+        ? [F, ...GetNumAndUnitTuple<R>]
+        : ['', ...GetNumAndUnitTuple<T>]
+    : ['', '', '']
