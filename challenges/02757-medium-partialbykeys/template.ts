78031e3a0216d2d803e803d062ff9bd73701524b
chenboowu <1028505817@qq.com>
2022-11-14 22:37:20 +0800

    chore: update

---
+++ challenges/02757-medium-partialbykeys/template.ts
@@ -0,0 +1,15 @@
+type PartialByKeys<T, K extends keyof T = keyof T> = ({
+    [Key in keyof T as Key extends K ? Key : never]? : T[Key]
+} & {
+    [Key in keyof T as Key extends K ? never : Key] : T[Key]
+}) extends infer R ? {
+    [Key in keyof R]: R[Key]
+} : never
+
+
+interface User {
+  name: string
+  age: number
+  address: string
+}
+type ts = PartialByKeys<User, 'name'>
