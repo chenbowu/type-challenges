e2117abcfd1d2374c452156f4be027029bb799d8
chenboowu <1028505817@qq.com>
2022-11-22 22:47:07 +0800

    chore: update

--- challenges/03243-medium-flattendepth/template.ts
+++ challenges/03243-medium-flattendepth/template.ts
@@ -1,1 +1,8 @@
-type FlattenDepth = any
+type FlattenDepth<T extends any[], D = 1, C extends 1[] = []> = 
+    C['length'] extends D 
+        ? T
+        : T extends [infer Head, ...infer Tail]
+            ? Head extends any[]
+                ? [...FlattenDepth<Head,  D, [...C, 1]>, ...FlattenDepth<Tail, D, C>]
+                : [Head, ...FlattenDepth<Tail, D, C>]
+            : []
