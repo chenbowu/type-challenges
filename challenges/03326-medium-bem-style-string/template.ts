ebbcd5af64a05843ba95070b3a822f6322ac2d0a
chenboowu <1028505817@qq.com>
2022-11-24 23:48:30 +0800

    chore: update

--- challenges/03326-medium-bem-style-string/template.ts
+++ challenges/03326-medium-bem-style-string/template.ts
@@ -1,1 +1,5 @@
-type BEM<B extends string, E extends string[], M extends string[]> = any
+type BEM<B extends string, E extends string[], M extends string[]> = BEM2<B, E[number], M[number]>
+
+type BEM2<B extends string, E extends string, M extends string> = `${B}${E extends string ? '__' : ''}${M extends string ? '--' : ''}`
+type tst = BEM<'btn', ['fsjdl'], []>
+type tsts = BEM2<'btn', 'fksdlj', 'flsdj'>
