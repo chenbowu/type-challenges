d162a0965a9a4adf5e92b849cccdf128fd179a42
chenboowu <1028505817@qq.com>
2022-11-26 23:51:09 +0800

    chore: update

--- challenges/03326-medium-bem-style-string/template.ts
+++ challenges/03326-medium-bem-style-string/template.ts
@@ -1,2 +1,3 @@
+// https://www.typescriptlang.org/docs/handbook/2/template-literal-types.html
 type WithPrefix<S extends string[], P extends string> = S extends [] ? '' : `${P}${S[number]}`
 type BEM<B extends string, E extends string[], M extends string[]> = `${B}${WithPrefix<E, '__'>}${WithPrefix<M, '--'>}`
