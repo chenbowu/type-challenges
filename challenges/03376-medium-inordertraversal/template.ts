65e843d669cc49a9d9b4384fb44cc4a5d7618f97
chenboowu <1028505817@qq.com>
2022-12-12 23:41:28 +0800

    chore: update

---
+++ challenges/03376-medium-inordertraversal/template.ts
@@ -0,0 +1,25 @@
+interface TreeNode {
+  val: number
+  left: TreeNode | null
+  right: TreeNode | null
+}
+type InorderTraversal<T extends TreeNode | null> = [T] extends [TreeNode]
+    ? [...InorderTraversal<T['left']>, T['val'], ...InorderTraversal<T['right']>]
+    : []
+
+// 为什么这里不转成元组就会报错
+// 因为条件类型左边如果是联合类型参数的话，会触发分布式条件类型，会将联合类型中每一个类型单独传入进行计算
+// [T] extends [TreeNode] 比较的是一个完整的联合类型，不会触发分布式条件类型
+// type InorderTraversal2<T extends TreeNode | null> = T extends TreeNode
+//     ? [...InorderTraversal2<T['left']>, T['val'], ...InorderTraversal2<T['right']>]
+//     : []
+
+const tree4 = {
+  val: 1,
+  left: null,
+  right: {
+    val: 2,
+    left: null,
+    right: null,
+  },
+} as const
