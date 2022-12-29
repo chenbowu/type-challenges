85ae36648de171b31e52dd25d3d312ce9aa15984
chenboowu <1028505817@qq.com>
2022-12-29 23:54:37 +0800

    chore: update

--- challenges/03376-medium-inordertraversal/template.ts
+++ challenges/03376-medium-inordertraversal/template.ts
@@ -4,6 +4,7 @@ interface TreeNode {
   right: TreeNode | null
 }
 
+
 type InorderTraversal<T extends TreeNode | null> = [T] extends [TreeNode]
     ? [...InorderTraversal<T['left']>, T['val'], ...InorderTraversal<T['right']>]
     : []
